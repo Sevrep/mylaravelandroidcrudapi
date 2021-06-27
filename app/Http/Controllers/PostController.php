7db01282096cc490f3acf8322894d3fa9c5ff092
Sevrep <62856034+Sevrep@users.noreply.github.com>
2021-06-28 06:48:36 +0800

    Add crud in post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,58 +7,29 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $post = Post::latest()->paginate(10);
+        return response()->json($post, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([ 'name' => 'required', ]);
+        $post = Post::create($request->all());
+        return response()->json($post, 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([ 'name' => 'required', ]);
+        $post->update($request->all());
+        return response()->json($post, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json("Post deleted.");
     }
 }
